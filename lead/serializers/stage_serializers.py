07c06723c67196859e03b01117580ede190e44f3
INPATHTAMILAN-M <inbarepute@gmail.com>
2024-12-10 17:06:16 +0530

    model u[pdate

--- lead/serializers/stage_serializers.py
+++ lead/serializers/stage_serializers.py
@@ -1,23 +1,23 @@
-from ..models import Stage
+from ..models import Log_Stage
 from rest_framework import serializers
 
 class GetStageSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Stage
+        model = Log_Stage
         fields = ['id', 'stage', 'description', 'probability', 'is_active']
 
 class PostStageSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Stage
+        model = Log_Stage
         fields = ['id', 'stage', 'description', 'probability', 'is_active']
 
 class PatchStageSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Stage
+        model = Log_Stage
         fields = ['id', 'stage', 'description', 'probability', 'is_active']
 
 
 class ListStageSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Stage
+        model = Log_Stage
         fields = ['id', 'stage', 'description', 'probability', 'is_active']
