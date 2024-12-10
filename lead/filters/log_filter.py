02b4e22da045398a440d3b67a2a397c53cbc6e0d
INPATHTAMILAN-M <inbarepute@gmail.com>
2024-12-10 15:45:06 +0530

    log filter update

--- lead/filters/log_filter.py
+++ lead/filters/log_filter.py
@@ -1,28 +1,23 @@
 
 import django_filters
 from django.db import models
-from django_filters import DateFilter, CharFilter, ChoiceFilter, ModelChoiceFilter, BooleanFilter
-from ..models import Log
+from ..models import Log,Lead,Contact,Opportunity,Focus_Segment,Log_Stage
 
 
 class LogFilter(django_filters.FilterSet):
-    start_date = DateFilter(field_name='created_on', lookup_expr='gte')
-    end_date = DateFilter(field_name='created_on', lookup_expr='lte')
-    contact_name = CharFilter(field_name='contact__name', lookup_expr='icontains')
-    lead_name = CharFilter(field_name='lead__name', lookup_expr='icontains')
-    opportunity_name = CharFilter(field_name='opportunity__name', lookup_expr='icontains')
-    details = CharFilter(field_name='details', lookup_expr='icontains')
-    logtype = ChoiceFilter(choices=[('Call', 'Call'), ('Meeting', 'Meeting'), ('Email', 'Email'), ('Others', 'Others')])
-    log_stage = ModelChoiceFilter(queryset=None)
-    focus_segment = ModelChoiceFilter(queryset=None)
-    is_active = BooleanFilter()
+    created_on = django_filters.DateFromToRangeFilter() 
+    
+    contact = django_filters.ModelChoiceFilter(queryset=Contact.objects.all())
+    lead = django_filters.ModelChoiceFilter(queryset=Lead.objects.all())
+    opportunity = django_filters.ModelChoiceFilter(queryset=Opportunity.objects.all())
+    details = django_filters.CharFilter(field_name='details', lookup_expr='icontains')
+    logtype = django_filters.ChoiceFilter(choices=[('Call', 'Call'), ('Meeting', 'Meeting'), ('Email', 'Email'), ('Others', 'Others')])
+    log_stage = django_filters.ModelChoiceFilter(queryset=Log_Stage.objects.all())
+    focus_segment = django_filters.ModelChoiceFilter(queryset=Focus_Segment.objects.all())
+    is_active = django_filters.BooleanFilter()
     
     class Meta:
         model = Log
         fields = ['contact', 'lead', 'opportunity', 'focus_segment', 'log_stage', 
                  'created_by', 'is_active', 'logtype']
     
-    def __init__(self, *args, **kwargs):
-        super(LogFilter, self).__init__(*args, **kwargs)
-        self.filters['log_stage'].queryset = Log_Stage.objects.all()
-        self.filters['focus_segment'].queryset = Focus_Segment.objects.all()
--- lead/viewsets/log_viewset.py
+++ lead/viewsets/log_viewset.py
@@ -5,11 +5,14 @@ from rest_framework.permissions import IsAuthenticated
 from ..custompagination import Paginator
 from ..models import Log
 from ..serializers.log_serializer import GetLogSerializer, PostLogSerializer, PatchLogSerializer, ListLogSerializer
+from ..filters import log_filter
+
 
 class LogViewSet(viewsets.ModelViewSet):
     queryset = Log.objects.all()
     # permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
+    filterset_class = log_filter.LogFilter
     pagination_class = Paginator
     alowed_methods = ['GET', 'POST', 'PATCH']
     
