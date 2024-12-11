8148bfce0a89e225c0e2523be46c433dc8f7cff7
INPATHTAMILAN-M <inbarepute@gmail.com>
2024-12-11 12:41:47 +0530

    task in paginator and authemticated

--- lead/viewsets/task_viewset.py
+++ lead/viewsets/task_viewset.py
@@ -1,10 +1,21 @@
 
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from ..models import Task, Task_Assignment
 from ..serializers.task_serializers import *
+from ..custompagination import Paginator
+from ..serializers.log_serializer import *
+from ..filters import log_filter
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = log_filter.LogFilter
+    pagination_class = Paginator
+    alowed_methods = ['GET', 'POST', 'PATCH']
     
     # def get_queryset(self):
     #     user = self.request.user
