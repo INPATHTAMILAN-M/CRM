6512be253e7f24152d53284e44f2a8f0e43eea4c
Mitun20 <mitung62@gmail.com>
2025-01-30 18:32:04 +0530

    update settings

--- crm/settings.py
+++ crm/settings.py
@@ -184,8 +184,8 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT={
-    'ACCESS_TOKEN_LIFETIME':timedelta(seconds=30),
-    'REFRESH_TOKEN_LIFETIME':timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME':timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME':timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,  
 }
 
