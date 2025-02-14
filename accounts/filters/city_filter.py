5ebc4dabba6bf4d44ef1b23a33fd408ca0b0b6c7
INPATHTAMILAN M <inbarepute@gmail.com>
2025-02-14 12:19:53 +0530

    update city filter

--- accounts/filters/city_filter.py
+++ accounts/filters/city_filter.py
@@ -1,7 +1,21 @@
 from django_filters import rest_framework as filters
 from ..models import City
+from django_filters.filters import OrderingFilter
 
 class CityFilter(filters.FilterSet):
+    
+    ordering = OrderingFilter(
+        fields=(
+            ('id', 'id'),  # Allows sorting by 'id'
+            ('city_name', 'city_name'),  # Sorting by 'city_name'
+            ('state', 'state'),  # Sorting by 'state'
+        ),
+        field_labels={
+            'id': 'ID',
+            'city_name': 'City Name',
+            'state': 'State',
+        }
+    )
 
     class Meta:
         model = City
