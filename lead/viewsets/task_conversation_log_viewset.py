1d31772df8a915cc5ff5883ee6f578f5b0ed2818
INPATHTAMILAN M <inbarepute@gmail.com>
2025-02-18 15:01:59 +0530

    update task conversation

--- lead/viewsets/task_conversation_log_viewset.py
+++ lead/viewsets/task_conversation_log_viewset.py
@@ -4,6 +4,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from ..filters.task_convo_filter import TaskConversationLogFilter
 from rest_framework.permissions import IsAuthenticated
 from django.db.models import Q
+from rest_framework.response import Response
 from ..serializers.task_conversation_log_serializer import (
     TaskConversationLogCreateSerializer,
     TaskConversationLogUpdateSerializer,
@@ -20,6 +21,7 @@ class TaskConversationLogViewSet(viewsets.ModelViewSet):
     filterset_class = TaskConversationLogFilter
     http_method_names = ['get', 'post', 'patch']
 
+    
     def get_serializer_class(self):
         if self.action == 'create':
             return TaskConversationLogCreateSerializer
@@ -29,3 +31,16 @@ class TaskConversationLogViewSet(viewsets.ModelViewSet):
             return TaskConversationLogUpdateSerializer
         return super().get_serializer_class()
     
+    def list(self, request, *args, **kwargs):
+        # Get the filtered queryset
+        queryset = self.filter_queryset(self.get_queryset())
+
+        # Update 'viewed=True' only for tasks assigned to or by the current user
+        queryset.filter(
+            Q(task__task_task_assignments__assigned_to=self.request.user) |
+            Q(task__task_task_assignments__assigned_by=self.request.user)
+        ).update(viewed=True)
+
+        # Continue with the normal list behavior
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
