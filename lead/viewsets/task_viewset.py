8e80fa9f3c5617cb50cae4e51f87891af22252ad
INPATHTAMILAN M <inbarepute@gmail.com>
2025-02-19 12:16:21 +0530

    update order by on task model

--- lead/viewsets/task_viewset.py
+++ lead/viewsets/task_viewset.py
@@ -12,7 +12,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all()
+    queryset = Task.objects.all().order_by('-task_conversation_logs__created_on')
     permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
     filterset_class = TaskFilter
@@ -22,9 +22,9 @@ class TaskViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         user = self.request.user
         if user.groups.filter(name='Admin').exists():
-            return Task.objects.all().order_by('-task_conversation_logs__created_on').distinct()
+            return Task.objects.all().distinct()
         task_ids = Task_Assignment.objects.filter(assigned_to=user).values_list("task", flat=True)
-        return Task.objects.filter(Q(id__in=task_ids) | Q(created_by=user)).order_by('-task_conversation_logs__created_on').distinct()    
+        return Task.objects.filter(Q(id__in=task_ids) | Q(created_by=user)).distinct()    
     
     def get_serializer_class(self):
         if self.action == 'create':
