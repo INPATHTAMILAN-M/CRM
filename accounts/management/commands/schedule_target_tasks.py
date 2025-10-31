5af833c89ec9e9a13b708d61f1766554a83c6294
INPATHTAMILAN M <inbarepute@gmail.com>
2025-10-31 16:24:15 +0530

    Refactor schedule_target_tasks command to calculate next run time for monthly adjustments

--- accounts/management/commands/schedule_target_tasks.py
+++ accounts/management/commands/schedule_target_tasks.py
@@ -1,7 +1,9 @@
 """
 Management command to schedule monthly target adjustment tasks with Django-Q
 """
+from datetime import datetime, timedelta, time
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 from django_q.tasks import schedule, Schedule
 
 
@@ -9,19 +11,24 @@ class Command(BaseCommand):
     help = 'Schedule monthly target adjustment tasks with Django-Q'
 
     def handle(self, *args, **options):
-        # Schedule monthly target adjustment to run every minute
+        now = timezone.now()
+        # Get first day of next month at 00:00
+        first_next_month = (now.replace(day=1) + timedelta(days=32)).replace(day=1)
+        next_run_time = datetime.combine(first_next_month, time.min).astimezone(
+            timezone.get_current_timezone()
+        )
+
         result = schedule(
             'accounts.tasks.adjust_monthly_targets',
             name='adjust_monthly_targets',
-            schedule_type=Schedule.MONTHLY, # I = minutes
+            schedule_type=Schedule.MONTHLY,
             months=1,
-            repeats=-1  # Repeat indefinitely
+            repeats=-1,
+            next_run=next_run_time
         )
-        
+
         self.stdout.write(
             self.style.SUCCESS(
-                f'Successfully scheduled adjust_monthly_targets (ID: {result})'
+                f'Successfully scheduled adjust_monthly_targets (ID: {result.id})'
             )
         )
-        
-        
