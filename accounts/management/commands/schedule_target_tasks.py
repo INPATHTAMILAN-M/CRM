1f468f57b2f45524fe1200b9885ff2d6e7a7ab52
INPATHTAMILAN M <inbarepute@gmail.com>
2025-10-31 16:05:02 +0530

    Refactor schedule_target_tasks command to use schedule function for monthly target adjustments

--- accounts/management/commands/schedule_target_tasks.py
+++ accounts/management/commands/schedule_target_tasks.py
@@ -2,64 +2,46 @@
 Management command to schedule monthly target adjustment tasks with Django-Q
 """
 from django.core.management.base import BaseCommand
-from django_q.models import Schedule
+from django_q.tasks import schedule, Schedule
 
 
 class Command(BaseCommand):
     help = 'Schedule monthly target adjustment tasks with Django-Q'
 
     def handle(self, *args, **options):
-        # Schedule monthly target adjustment to run at the end of each month
-        schedule, created = Schedule.objects.update_or_create(
+        # Schedule monthly target adjustment to run every minute
+        result = schedule(
+            'accounts.tasks.adjust_monthly_targets',
             name='adjust_monthly_targets',
-            defaults={
-                'func': 'accounts.tasks.adjust_monthly_targets',
-                'schedule_type': Schedule.MINUTES,
-                'next_run': None,  # Will be calculated automatically
-                'repeats': -1,  # Repeat indefinitely
-            }
+            schedule_type=Schedule.MINUTES, # I = minutes
+            minutes=1,
+            repeats=-1  # Repeat indefinitely
         )
-        
-        if created:
-            self.stdout.write(
-                self.style.SUCCESS(
-                    'Successfully created schedule for adjust_monthly_targets'
-                )
-            )
-        else:
-            self.stdout.write(
-                self.style.SUCCESS(
-                    'Updated existing schedule for adjust_monthly_targets'
-                )
-            )
-        
-        # You can also schedule a daily recalculation if needed
-        schedule2, created2 = Schedule.objects.update_or_create(
-            name='recalculate_all_monthly_targets',
-            defaults={
-                'func': 'accounts.tasks.recalculate_all_monthly_targets',
-                'schedule_type': Schedule.DAILY,
-                'next_run': None,
-                'repeats': -1,
-            }
-        )
-        
-        if created2:
-            self.stdout.write(
-                self.style.SUCCESS(
-                    'Successfully created schedule for recalculate_all_monthly_targets'
-                )
-            )
-        else:
-            self.stdout.write(
-                self.style.SUCCESS(
-                    'Updated existing schedule for recalculate_all_monthly_targets'
-                )
-            )
         
         self.stdout.write(
             self.style.SUCCESS(
-                '\nSchedules created! Make sure Django-Q cluster is running:\n'
-                'python manage.py qcluster'
+                f'Successfully scheduled adjust_monthly_targets (ID: {result})'
             )
         )
+        
+        # # Schedule daily recalculation
+        # result2 = schedule(
+        #     'accounts.tasks.recalculate_all_monthly_targets',
+        #     name='recalculate_all_monthly_targets',
+        #     schedule_type=Schedule.DAILY,
+        #     minutes=1440,
+        #     repeats=-1
+        # )
+        
+        self.stdout.write(
+            self.style.SUCCESS(
+                f'Successfully scheduled recalculate_all_monthly_targets (ID: {result2})'
+            )
+        )
+        
+        # self.stdout.write(
+        #     self.style.SUCCESS(
+        #         '\nSchedules created! Make sure Django-Q cluster is running:\n'
+        #         'python manage.py qcluster'
+        #     )
+        # )
