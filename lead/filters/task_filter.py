c5b5404581b789e199d0e04eb4f7b2ed4d18e799
hariharan <suryahariharan512@gmail.com>
2025-11-13 12:05:38 +0530

    Fix TaskFilter to correctly apply method for assigned_to field filtering

--- lead/filters/task_filter.py
+++ lead/filters/task_filter.py
@@ -14,7 +14,7 @@ class TaskFilter(filters.FilterSet):
     from_date = filters.DateFilter(field_name='task_date_time', lookup_expr='gte', label='From Date')
     to_date = filters.DateFilter(field_name='task_date_time', lookup_expr='lte', label='To Date', required=False)
     assigned_to_me = filters.BooleanFilter(field_name='task_task_assignments__assigned_to', method='filter_assigned_to_me')
-    assigned_to = filters.NumberFilter(field_name='task_task_assignments__assigned_to', label='Assigned To User ID')
+    assigned_to = filters.NumberFilter(field_name='task_task_assignments__assigned_to', method='filter_assigned_to_me', label='Assigned To User ID')
     assigned_by_me = filters.BooleanFilter(field_name='task_task_assignments__assigned_by', method='filter_assigned_by_me')
     
     has_reply = filters.BooleanFilter(field_name='task_conversation_logs__task', method='filter_has_reply')
